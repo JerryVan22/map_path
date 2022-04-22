d3e16983b1c82c022970ee086cf8a0daad8c7f91
ruiqi van <van1022@139.com>
2022-04-22 20:35:16 +0800

    delete myenv

---
+++ model/optimal_path_model.py
@@ -0,0 +1,10 @@
+from model.weather_model import Weather
+class Dijkstra_Algorithm():
+    def __init__(self,map_data) -> None:
+        self.map_data=map_data
+
+    def solve(self,weather,trip_mode,place_of_departure,destination):
+        pass
+        
+
+
